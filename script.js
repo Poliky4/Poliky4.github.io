cc9da9ca9899e55edb8a66d2a4a6a49d7571ede9
Poliky4 <pontusmolin@me.com>
2017-08-03 11:23:45 +0200

    made http request into one way only, no response

--- script.js
+++ script.js
@@ -21,12 +21,13 @@ pages.forEach(utils.setOnClickNavBtns);
 utils.hidePages();      // hide all pages
 utils.showPage(home);   // and show home
 
-// http request to my Raspberry Pi to log visits
+// one way http request to my Raspberry Pi to log visits
 let webstatsData = {time: new Date(), browser: navigator.appName, platform: navigator.platform}
 let webstatsRequest = new XMLHttpRequest();
-webstatsRequest.open('POST', 'http://89.236.60.99:1337/webstats', true);
+webstatsRequest.open('POST', 'http://localhost:1337/webstats', true); // true for async
 webstatsRequest.setRequestHeader("Content-Type", "text/plain");
 webstatsRequest.send(JSON.stringify(webstatsData));
+webstatsRequest.abort(); // this is what makes it one way
 
 function Utils() {
     
